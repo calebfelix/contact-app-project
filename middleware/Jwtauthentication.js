267ff00d51da50ffc6109e5b6c16b422dc7bc810
caleb <calebfelix38@gmail.com>
2023-09-19 20:14:59 +0530

    done2

--- middleware/Jwtauthentication.js
+++ middleware/Jwtauthentication.js
@@ -46,8 +46,6 @@ class Jwtauthentication {
           throw new UnauthorizedError("Token Not Found");
         }
         let payload = Jwtauthentication.verifyToken(token);
-        console.log(req.params.userId)
-        console.log(payload)
         if(payload.userId != req.params.userId){
           throw new UnauthorizedError("User does not access");
         }
@@ -65,8 +63,6 @@ class Jwtauthentication {
           throw new UnauthorizedError("Token Not Found");
         }
         let {userId, contactId} = req.params
-        console.log(userId)
-        console.log(contactId)
         let result = await db.contact.findAll({where:{id:contactId, user_id:userId}}) 
         if(result.length == 0){
           throw new NotFoundError("Contact Not Found with this user");
@@ -118,23 +114,5 @@ class Jwtauthentication {
     }
   }
 
-  static isActive(req, res, next) {
-    try {
-      console.log("isActive Started");
-      const token = req.cookies.auth;
-      if (!token) {
-        throw new UnauthorizedError("Token Not Found");
-      }
-      let payload = Jwtauthentication.verifyToken(token);
-      if (payload.isActive) {
-        next();
-        return
-      } else {
-        throw new NotFoundError("User dosen't exist")
-      }
-    } catch (error) {
-      next(error);
-    }
-  }
 }
 module.exports = Jwtauthentication;
